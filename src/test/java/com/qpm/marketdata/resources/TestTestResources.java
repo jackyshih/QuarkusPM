f4dbc74bc04440c55a2ed3c1902e170438b3c0fa
jackyshih <jacky.shih@gmail.com>
2020-07-12 21:03:49 +0800

    new test case failure

--- src/test/java/com/qpm/marketdata/resources/TestTestResources.java
+++ src/test/java/com/qpm/marketdata/resources/TestTestResources.java
@@ -9,7 +9,7 @@ public class TestTestResources {
 
         // assert statements
         assertEquals(0, 0, "10 x 0 must be 0");
-        //assertEquals(0, tester.multiply(0, 10), "0 x 10 must be 0");
+        assertEquals(0, 1, "0 x 10 must be 0");
         //assertEquals(0, tester.multiply(0, 0), "0 x 0 must be 0");
     }
 }
